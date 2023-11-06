ee62d2c3edf762a5b03db0ee15725f5ca3748746
emilvn <emilvnielsen@hotmail.com>
2023-11-06 12:03:39 +0100

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
