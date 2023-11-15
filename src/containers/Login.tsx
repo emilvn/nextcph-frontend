26c327a80ad2ec773dd352e7814f7ffc78a58646
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-11-15 12:56:11 +0100

    styling not finished

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -2,17 +2,24 @@ import {SignInButton} from "@clerk/clerk-react";
 
 function Login() {
     return (
-        <div className="flex justify-around me-auto p-2 items-center min-h-screen">
-            <SignInButton mode={"modal"}>
-                <button className="rounded bg-next-darker-orange p-2">
-                    Log ind som Admin
-                </button>
-            </SignInButton>
-            <SignInButton mode={"modal"}>
-                <button className="rounded bg-next-darker-orange p-2">
-                    Log ind
-                </button>
-            </SignInButton>
+        <div className="flex items-center justify-around me-auto min-h-screen">
+            <div className="rounded-2xl shadow-sm border-box border w-60">
+                <img className="w-full rounded" src="../../public/images/Admin.png" alt=""/>
+                <SignInButton mode={"modal"}>
+                    <button className="mt-20 rounded bg-next-darker-orange p-2 mx-11">
+                        Log ind
+                    </button>
+                </SignInButton>
+            </div>
+            <div className="rounded-2xl shadow-sm border-box border w-60 p-10">
+                <img src="../../public/images/Frisør-lærling.png" alt=""/>
+                <img src="../../public/images/Kosemitik-lærling.png" alt=""/>
+                <SignInButton mode={"modal"}>
+                    <button className="rounded bg-next-darker-orange p-2 m-10">
+                        Log ind
+                    </button>
+                </SignInButton>
+            </div>
         </div>
     );
 }
