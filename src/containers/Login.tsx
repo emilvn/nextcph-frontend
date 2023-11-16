9d25a78f6985daab4e405174b0e6ce617db2a465
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-11-16 08:34:52 +0100

    only one login button

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -3,32 +3,16 @@ import {SignInButton} from "@clerk/clerk-react";
 function Login() {
     return (
         <div
-            className="flex items-center justify-around min-h-screen">
+            className="flex items-center justify-center min-h-screen bg-next-orange opacity-80">
             <div className="flex-row max-w-4xl mx-4">
-                <div className="relative overflow-clip">
-                    <img className="w-full h-[40rem] rounded" src="/images/Admin.png" alt="Card Image"/>
+                <div className="relative overflow-clip border-black border-2">
+                    <img className="w-full h-[30rem] rounded" src="/images/Admin.png" alt="Card Image"/>
 
                     <div className="absolute w-full bottom-0 flex justify-center items-center p-10">
                         <SignInButton mode={"modal"}>
                             <button
-                                className="bg-next-darker-orange text-white rounded-xl p-4 text-2xl w-60">
+                                className="bg-next-darker-orange text-white rounded-xl p-4 text-2xl w-60 hover:bg-next-orange">
                                 Log ind Admin
-                            </button>
-                        </SignInButton>
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex-row max-w-4xl mx-4">
-                <div className="relative">
-                    <img className="w-full h-[40rem] rounded" src="/images/Kosmetik-Frisør.png"
-                         alt="Card Image"/>
-
-                    <div className="absolute w-full bottom-0 flex justify-center items-center p-10">
-                        <SignInButton mode={"modal"}>
-                            <button
-                                className="bg-next-blue text-white rounded-xl p-4 text-2xl w-60 border border-slate-200">
-                                Log ind
                             </button>
                         </SignInButton>
                     </div>
