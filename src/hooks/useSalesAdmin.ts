9e08b54b4a0741bbc59fa2833451f371e90b5bc2
emilvn <emilvnielsen@hotmail.com>
2023-11-17 11:13:00 +0100

    initial useSales hooks

---
+++ src/hooks/useSalesAdmin.ts
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import type {INewSale, ISale} from "../types/sales.types.ts";
+import type {ChannelType} from "../types/channel.types.ts";
+import SaleApi from "../utils/SaleApi.ts";
+
+function useSalesAdmin(channel:ChannelType){
+	const [sales, setSales] = useState<ISale[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const api = new SaleApi();
+
+	useEffect(() => {
+		async function loadSales() {
+			try{
+				const sales = await api.getByChannel(channel);
+				setSales(sales);
+			}
+			catch (e) {
+				console.error(e);
+			}
+		}
+
+		loadSales().then(() => setIsLoading(false));
+	});
+
+	const create = async (sale:INewSale) => {
+		try{
+			const newSale = await api.create(sale);
+			const newSales = [...sales, newSale];
+			setSales(newSales);
+		}catch(e){
+			console.error(e);
+		}
+	}
+
+	const destroy = async (sale:ISale) => {
+		try{
+			await api.deleteById(sale.id);
+			const index = sales.findIndex((s) => s.id === sale.id);
+			sales.splice(index, 1);
+			setSales([...sales]);
+		} catch(e){
+			console.error(e);
+		}
+	}
+
+	return {sales, isLoading, create, destroy};
+}
+
+export default useSalesAdmin;
