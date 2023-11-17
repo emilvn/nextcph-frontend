26b2738e44ca53196293247f4bb8eaf7e718cb7c
emilvn <emilvnielsen@hotmail.com>
2023-11-17 13:47:54 +0100

    removed test funcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import {Test} from "./Test.tsx";
-import {SignInButton, useUser} from "@clerk/clerk-react";
 
 function App() {
-    const {isSignedIn, isLoaded:userIsLoaded, user} = useUser();
-
-    if(!userIsLoaded) return (<div>Loading...</div>);
-    if(!isSignedIn) return (<div>
-        <SignInButton>
-            <button>
-                Sign up
-            </button>
-        </SignInButton>
-    </div>);
-    if(!user || !user.id) return (<div>Something went wrong</div>);
-
     return (
         <>
-            <Test user_id={user.id}/>
+            <div>delet this</div>
         </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
-import {ClerkProvider} from "@clerk/clerk-react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <ClerkProvider publishableKey={import.meta.env.VITE_APP_CLERK_PUBLISHABLE_KEY}>
-            <App />
-      </ClerkProvider>
+        <App />
   </React.StrictMode>,
 )
