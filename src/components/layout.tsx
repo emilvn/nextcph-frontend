414ba58fa8f80be0e6f2a273776411116452a59d
emilvn <emilvnielsen@hotmail.com>
2023-11-20 23:02:37 +0100

    minor

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ function Footer() {
 		<footer className="fixed bottom-0 left-0 right-0 h-[calc(100vh-80px)] text-next-grey flex items-center justify-evenly mr-20 ml-20 p-4 gap-8" >
 			<div className="w-80">
 				<h3 className="text-xl text-next-blue font-bold">VEMC</h3>
-				<p className="text-next-grey">Vi er en gruppe af 4 studerende fra Københavns Erhvervsakademi, der har lavet denne hjemmeside som en del af vores 2. semester projekt.</p>
+				<p className="text-next-grey">Vi er en gruppe af 4 studerende fra Københavns Erhvervsakademi, der har lavet denne hjemmeside som en del af vores førsteårs projekt.</p>
 			</div>
 			<div>
 				<h3 className="text-xl text-next-blue font-bold">KONTAKT</h3>
