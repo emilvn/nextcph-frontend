3f70e43b3918d8a2bc58363147df3af6ef76cdab
emilvn <emilvnielsen@hotmail.com>
2023-11-22 00:03:53 +0100

    styling

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -9,24 +9,24 @@ function Login() {
             <PageLayout>
                 <div className="flex items-center bg-[url('/images/Admin.png')] w-full min-h-screen bg-cover bg-center">
                     <div className="flex max-md:flex-col justify-center items-center max-md:h-[75vh] h-[50vh] w-full">
-                        <div className="flex gap-8 flex-col h-[50vh] w-full justify-center items-center bg-next-blue">
-                            <h2 className="text-next-darker-orange text-3xl font-bold">
-                                Kom igang med at sælge i dag
+                        <div className="flex gap-8 flex-col h-[50vh] w-full justify-center bg-next-blue">
+                            <h2 className="text-next-darker-orange text-3xl font-bold md:ml-8">
+                                Kom i gang med at sælge i dag
                             </h2>
                             <SignUpButton mode={"modal"}>
                                     <button
-                                        className="bg-next-blue border-2 border-next-darker-orange text-next-darker-orange font-bold p-4 text-2xl w-60 hover:bg-next-darker-orange hover:text-next-blue transition-colors">
+                                        className="bg-next-blue border-[1.5px] border-next-darker-orange text-next-darker-orange font-bold p-4 text-2xl w-60 hover:bg-next-darker-orange hover:text-next-blue transition-colors md:ml-8">
                                         Opret bruger
                                     </button>
                             </SignUpButton>
                         </div>
-                        <div className="flex gap-8 h-[50vh] flex-col w-full justify-center items-center bg-next-darker-orange">
-                            <h2 className="text-next-blue text-3xl font-bold">
+                        <div className="flex gap-8 h-[50vh] flex-col w-full justify-center bg-next-darker-orange">
+                            <h2 className="text-next-blue text-3xl font-bold md:ml-8">
                                 Har du allerede en bruger?
                             </h2>
                             <SignInButton mode={"modal"}>
                                 <button
-                                    className="bg-next-darker-orange text-next-blue font-bold p-4 text-2xl w-60 hover:bg-next-blue hover:text-next-darker-orange border-2 border-next-blue transition-colors">
+                                    className="md:ml-8 bg-next-darker-orange text-next-blue font-bold p-4 text-2xl w-60 hover:bg-next-blue hover:text-next-darker-orange border-[1.5px] border-next-blue transition-colors">
                                     Log ind
                                 </button>
                             </SignInButton>
