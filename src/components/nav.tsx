cbc63f28cb8465b74dadd0d408fb6f9ad832a1df
emilvn <emilvnielsen@hotmail.com>
2023-11-21 18:23:33 +0100

    indents

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -25,23 +25,23 @@ function NavBar({children}:PropsWithChildren) {
 		<>
 			<nav className="z-20 fixed left-0 top-0 h-screen w-20 bg-next-white"></nav>
 			<nav className="z-20 fixed top-0 left-20 w-[calc(100%-80px)] h-20 bg-next-white flex items-center text-next-grey justify-between">
-					<div className="font-mono font-bold text-5xl text-next-blue select-none mr-5">vem<span className='text-next-orange'>c</span></div>
-					<div className="flex w-full h-full">
-						{children}
-					</div>
-					<div className="flex-shrink-0 h-full w-20 flex items-center justify-center bg-next-blue text-next-darker-orange hover:bg-next-darker-orange hover:text-next-blue cursor-pointer">
-						<SignedIn>
-							<UserButton afterSignOutUrl="/"/>
-						</SignedIn>
-						<SignedOut>
-							<SignInButton mode={"modal"}>
-								<div className="h-full w-full flex justify-center items-center" onMouseEnter={()=> setLocked(false)} onMouseLeave={() => setLocked(true)}>
-									{!!locked && <FaLock/>}
-									{!locked && <FaUnlock/>}
-								</div>
-							</SignInButton>
-						</SignedOut>
-					</div>
+				<div className="font-mono font-bold text-5xl text-next-blue select-none mr-5">vem<span className='text-next-orange'>c</span></div>
+				<div className="flex w-full h-full">
+					{children}
+				</div>
+				<div className="flex-shrink-0 h-full w-20 flex items-center justify-center bg-next-blue text-next-darker-orange hover:bg-next-darker-orange hover:text-next-blue cursor-pointer">
+					<SignedIn>
+						<UserButton afterSignOutUrl="/"/>
+					</SignedIn>
+					<SignedOut>
+						<SignInButton mode={"modal"}>
+							<div className="h-full w-full flex justify-center items-center" onMouseEnter={()=> setLocked(false)} onMouseLeave={() => setLocked(true)}>
+								{!!locked && <FaLock/>}
+								{!locked && <FaUnlock/>}
+							</div>
+						</SignInButton>
+					</SignedOut>
+				</div>
 			</nav>
 			<nav className="z-20 fixed right-0 top-20 h-screen w-20 bg-next-white"></nav>
 		</>
