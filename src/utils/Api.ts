2577fc2005d4f7bccb930c1b6b1a706a462cb88f
emilvn <emilvnielsen@hotmail.com>
2023-11-21 08:50:37 +0100

    added backend env variable

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -2,7 +2,7 @@ import {ChannelType} from "../types/channel.types.ts";
 
 abstract class Api<DataType,NewDataType>{
 	protected abstract url: string;
-	public readonly endpoint = "http://localhost:3333";
+	public readonly endpoint = import.meta.env.VITE_BACKEND_URL || "http://localhost:3333";
 	abstract getByChannel(channel: ChannelType): Promise<DataType[]>;
 	abstract getById(id: string): Promise<DataType>;
 	abstract create(data: NewDataType): Promise<DataType>;
