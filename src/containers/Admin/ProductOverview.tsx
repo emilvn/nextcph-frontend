8fd6f70758ad5e764ffc2f4f6b94d83d30147709
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-11-22 15:27:33 +0100

    update the delete and createt the modal window

--- src/containers/Admin/ProductOverview.tsx
+++ src/containers/Admin/ProductOverview.tsx
@@ -1,30 +1,65 @@
-// ProductOverview.tsx
 import PageLayout from '../../components/layout.tsx';
-import { ChannelType } from '../../types/channel.types.ts';
+import {ChannelType} from '../../types/channel.types.ts';
 import useProducts from '../../hooks/useProducts.ts';
-import { IProduct, INewProduct } from '../../types/products.types.ts';
+import {IProduct} from '../../types/products.types.ts';
 import loading from '../../components/loading.tsx';
-import { useState } from 'react';
-function ProductOverview({ channel }: { channel: ChannelType }) {
-    const { products, isLoading, destroy, create } = useProducts(channel);
+import {useState} from 'react';
+
+function ProductOverview({channel}: { channel: ChannelType }) {
+    const {products, isLoading, destroy, create} = useProducts(channel);
     const [showModal, setShowModal] = useState(false);
 
     const handleDelete = (product: IProduct) => {
         void destroy(product);
     };
 
-    const handleCreate = async (newProduct: INewProduct) => {
-        await create(newProduct, INewProduct);
-        setShowModal(false);
-    };
-
     return (
         <PageLayout>
             <div className="my-4">
-                <h2 className="text-2xl font-bold mb-2">Product List:</h2>
-                <button onClick={() => setShowModal(true)}>Create Product</button>
+                <h2 className="text-3xl font-bold text-indigo-600 mb-4">Product List:</h2>
+                <button
+                    className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
+                    onClick={() => setShowModal(true)}
+                >
+                    Create Product
+                </button>
+
+                {showModal && (
+                    <div className="fixed inset-0 flex items-center justify-center">
+                        <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
+                        <div className="bg-white p-8 rounded-md relative z-10">
+                            <form>
+                                <label className="block mb-4">
+                                    Name: <input className="border border-gray-300 p-2 w-full" type="text" name="name"/>
+                                </label>
+                                <label className="block mb-4">
+                                    Price: <input className="border border-gray-300 p-2 w-full" type="number"
+                                                  name="price"/>
+                                </label>
+                                <label className="block mb-4">
+                                    Stock: <input className="border border-gray-300 p-2 w-full" type="number"
+                                                  name="stock"/>
+                                </label>
+
+                                <button
+                                    className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                                    type="submit"
+                                >
+                                    Create
+                                </button>
+                            </form>
+                            <button
+                                className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 mt-4"
+                                onClick={() => setShowModal(false)}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                )}
+
                 {isLoading ? (
-                    <loading.LoadingSpinner />
+                    <loading.LoadingSpinner/>
                 ) : (
                     <table className="min-w-full">
                         <thead>
@@ -43,7 +78,12 @@ function ProductOverview({ channel }: { channel: ChannelType }) {
                                     <td className="py-2">{product.price}</td>
                                     <td className="py-2">{product.stock}</td>
                                     <td>
-                                        <button onClick={() => handleDelete(product)}>Delete</button>
+                                        <button
+                                            className="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-600"
+                                            onClick={() => handleDelete(product)}
+                                        >
+                                            Delete
+                                        </button>
                                     </td>
                                 </tr>
                             ))}
@@ -51,18 +91,6 @@ function ProductOverview({ channel }: { channel: ChannelType }) {
                     </table>
                 )}
             </div>
-
-            {showModal && (
-                <div className="modal">
-                    <div className="modal-content">
-            <span className="close" onClick={() => setShowModal(false)}>
-              &times;
-            </span>
-                        <h2>Create New Product</h2>
-                        <button onClick={() => handleCreate({/* pass product details here */})}>Create</button>
-                    </div>
-                </div>
-            )}
         </PageLayout>
     );
 }
