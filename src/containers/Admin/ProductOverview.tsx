406a2875bd87be1a6fc479a87aed6cfb7db468da
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-11-22 09:49:36 +0100

    init

--- src/containers/Admin/ProductOverview.tsx
+++ src/containers/Admin/ProductOverview.tsx
@@ -1,12 +1,13 @@
 import PageLayout from "../../components/layout.tsx";
 import {ChannelType} from "../../types/channel.types.ts";
 
-function ProductOverview({channel}:{channel:ChannelType}) {
-  return (
-    <PageLayout>
-      ProductOverview - {channel}
-    </PageLayout>
-  )
+function ProductOverview({channel}: { channel: ChannelType }) {
+    return (
+        <PageLayout>
+            ProductOverview - {channel}
+            {/*klsdfjkldjflkds*/}
+        </PageLayout>
+    )
 }
 
 export default ProductOverview
