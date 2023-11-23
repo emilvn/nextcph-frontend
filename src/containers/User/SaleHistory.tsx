c6591b359644ed7afc4c6bdb436a763b741a358c
Bindholt <Cbindholt@gmail.com>
2023-11-23 14:05:50 +0100

    stuff

--- src/containers/User/SaleHistory.tsx
+++ src/containers/User/SaleHistory.tsx
@@ -1,12 +1,28 @@
 import PageLayout from "../../components/layout.tsx";
-import type {ChannelType} from "../../types/channel.types.ts";
+import type { ChannelType } from "../../types/channel.types.ts";
+import useSalesUser from "../../hooks/useSalesUser.ts";
+import Loading from "../../components/loading.tsx";
+import { ISale } from "../../types/sales.types.ts";
 
-function SaleHistory({channel}:{channel:ChannelType}) {
+function Sales({ sales }: { sales: ISale[] }) {
+	return <div>et salg</div>
+}
+
+function SaleHistory({ channel }: { channel: ChannelType }) {
+
+	const { sales, isLoading } = useSalesUser(channel);
+	console.log(sales);
+	if (isLoading) return (<Loading.LoadingPage />);
+	if (!sales || sales.length === 0) return (<PageLayout>No sales found...</PageLayout>);
+
 	return (
 		<PageLayout>
-			SaleHistory - {channel}
+			<div>
+				<Sales sales={sales} />
+			</div>
 		</PageLayout>
 	);
 }
 
 export default SaleHistory
+
--- src/hooks/useSalesUser.ts
+++ src/hooks/useSalesUser.ts
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react";
-import type {INewSale, ISale} from "../types/sales.types.ts";
-import type {ChannelType} from "../types/channel.types.ts";
+import { useEffect, useState } from "react";
+import type { INewSale, ISale } from "../types/sales.types.ts";
+import type { ChannelType } from "../types/channel.types.ts";
 import SaleApi from "../utils/SaleApi.ts";
 
-function useSalesUser(channel:ChannelType, user_id:string){
+function useSalesUser(channel: ChannelType) {
 	const [sales, setSales] = useState<ISale[]>([]);
 	const [isLoading, setIsLoading] = useState(true);
 
@@ -11,8 +11,8 @@ function useSalesUser(channel:ChannelType, user_id:string){
 
 	useEffect(() => {
 		async function loadSales() {
-			try{
-				const sales = await api.getByUserId(user_id, channel);
+			try {
+				const sales = await api.getByChannel(channel);
 				setSales(sales);
 			}
 			catch (e) {
@@ -23,17 +23,17 @@ function useSalesUser(channel:ChannelType, user_id:string){
 		loadSales().then(() => setIsLoading(false));
 	}, []);
 
-	const create = async (sale:INewSale) => {
-		try{
+	const create = async (sale: INewSale) => {
+		try {
 			const newSale = await api.create(sale);
 			const newSales = [...sales, newSale];
 			setSales(newSales);
-		}catch(e){
+		} catch (e) {
 			console.error(e);
 		}
 	}
 
-	return {sales, isLoading, create};
+	return { sales: sales, isLoading, create };
 }
 
 export default useSalesUser;
