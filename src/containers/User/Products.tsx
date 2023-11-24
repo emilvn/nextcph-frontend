ba74d2397323fc682cb73c57e2dc69280bcfac6a
emilvn <emilvnielsen@hotmail.com>
2023-11-24 13:30:26 +0100

    search

--- src/containers/User/Products.tsx
+++ src/containers/User/Products.tsx
@@ -6,16 +6,25 @@ import {IProduct} from "../../types/products.types.ts";
 import {IoSearchOutline } from "react-icons/io5";
 import {getCategories, getProductsWithCategory} from "../../helpers/categories.ts";
 import {FaMinus, FaPlus} from "react-icons/fa";
+import {Dispatch, SetStateAction, useState} from "react";
 
 const channelDict = {
 	"HAIR_CARE": "FRISØR",
 	"COSMETIC": "KOSMETIKER"
 }
 
-function SearchBar({products}:{products:IProduct[]}) {
+interface searchProps {
+	setSearch: Dispatch<SetStateAction<string>>;
+}
+function SearchBar({setSearch}:searchProps) {
 	return (
 		<div className="flex items-center">
-			<input type="text" placeholder="SØG EFTER PRODUKT" className="p-4 outline-none w-80 placeholder:text-next-blue placeholder:font-semibold"/>
+			<input
+				type="text"
+				placeholder="SØG EFTER PRODUKT"
+				className="p-4 outline-none w-80 placeholder:text-next-blue placeholder:font-semibold"
+				onChange={(e) => setSearch(e.target.value)}
+			/>
 			<div className="bg-white p-4">
 				<IoSearchOutline className="text-next-blue w-full text-2xl"/>
 			</div>
@@ -34,43 +43,47 @@ function NavCategory({category}:{category:string}) {
 	);
 }
 
-function CategoriesNav({products}:{products:IProduct[]}) {
-	const categories = getCategories(products);
+function CategoriesNav({categories}:{categories:string[]}) {
 	return (
 		<div className="flex flex-col gap-4 items-start">
-			{categories.map((category) => (<NavCategory category={category}/>))}
+			{categories.map((category) => (<NavCategory key={category} category={category}/>))}
 		</div>
 	);
 }
 
-function SearchAndFilter({products}:{products:IProduct[]}) {
+interface searchAndFilterProps {
+	categories: string[];
+	setSearch: Dispatch<SetStateAction<string>>;
+}
+function SearchAndFilter({categories, setSearch}:searchAndFilterProps) {
 	return (
 		<div className="flex flex-col items-center fixed bg-next-blue h-screen right-20 top-20 p-2 gap-8">
-			<SearchBar products={products}/>
-			<CategoriesNav products={products}/>
+			<SearchBar setSearch={setSearch}/>
+			<CategoriesNav categories={categories}/>
 		</div>
 	);
 }
 
 interface IHeaderProps {
 	channel: ChannelType;
-	products: IProduct[];
+	categories: string[];
+	setSearch: Dispatch<SetStateAction<string>>;
 }
-function Header({channel, products}:IHeaderProps) {
+function Header({channel, categories, setSearch}:IHeaderProps) {
 
 	return (
 		<div className="fixed left-20 right-20 top-20 z-20">
 			<div className="bg-next-blue flex items-center justify-between gap-8 p-5 h-[79px]">
 				<h2 className="text-next-orange text-3xl font-bold">{channelDict[channel]} PRODUKTER</h2>
 			</div>
-			<SearchAndFilter products={products}/>
+			<SearchAndFilter categories={categories} setSearch={setSearch}/>
 		</div>
 	);
 }
 
 function Product({product}:{product:IProduct}) {
 	return(
-		<div className="p-4 w-[calc(100%-1px)] bg-next-white flex flex-col gap-4">
+		<div className="p-4 w-full bg-next-white flex flex-col gap-4">
 			<div className="flex justify-between">
 				<div className="flex flex-col text-lg font-semibold">
 					<h3 className="text-2xl font-bold">{product.name}</h3>
@@ -78,10 +91,10 @@ function Product({product}:{product:IProduct}) {
 					<p>Pris: {product.price},-</p>
 				</div>
 				<div className="flex flex-col gap-2">
-					<button className="border border-next-blue p-2 text-next-blue text-xl font-semibold hover:bg-next-blue hover:text-next-darker-orange transition-colors">
+					<button className="border border-next-blue p-2 text-next-blue text-xl font-semibold hover:bg-next-blue hover:text-next-white transition-colors">
 						<FaPlus className="text-xl inline-block"/> Tilføj til salg
 					</button>
-					<button className="border border-next-blue p-2 text-next-blue text-xl font-semibold hover:bg-next-blue hover:text-next-darker-orange transition-colors">
+					<button className="border border-next-blue p-2 text-next-blue text-xl font-semibold hover:bg-next-blue hover:text-next-white transition-colors">
 						<FaMinus className="text-xl inline-block"/> Fjern fra salg
 					</button>
 				</div>
@@ -92,10 +105,10 @@ function Product({product}:{product:IProduct}) {
 
 function Category({category, products}:{category:string, products:IProduct[]}) {
 	return(
-		<div id={category} className="scroll-mt-40">
+		<div id={category} className="scroll-mt-40 w-full">
 			<h2 className="text-2xl p-4 font-bold bg-next-blue mb-[1px] w-full text-next-darker-orange text-center">{category}</h2>
-			<div className="flex flex-wrap w-full gap-[1px]">
-				{products.map((product:IProduct) => (<Product product={product}/>))}
+			<div className="flex flex-col w-full gap-[1px]">
+				{products.map((product:IProduct) => (<Product key={product.id} product={product}/>))}
 			</div>
 		</div>
 	);
@@ -103,19 +116,24 @@ function Category({category, products}:{category:string, products:IProduct[]}) {
 
 function Products({channel}:{channel:ChannelType}) {
 	const {products, isLoading} = useProducts(channel);
+	const [search, setSearch] = useState("");
 	if(isLoading) return (<Loading.LoadingPage/>);
 	if(!products || products.length === 0) return (<PageLayout>No products found...</PageLayout>);
-	const categories = getCategories(products);
+
+	const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(search.toLowerCase()));
+
+	const categories = getCategories(filteredProducts);
 
 	return (
         <PageLayout>
-			<Header channel={channel} products={products}/>
+			<Header channel={channel} categories={categories} setSearch={setSearch}/>
 			<div className="mt-40 flex w-full">
-				<div className="flex flex-col gap-[1px]">
+				<div className="flex flex-col gap-[1px] w-full">
 					{categories.map((category) => (
 						<Category
+							key={category}
 							category={category}
-							products={getProductsWithCategory(products, category)}
+							products={getProductsWithCategory(filteredProducts, category)}
 						/>
 						))}
 				</div>
