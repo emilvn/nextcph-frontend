5d52829c39deca970bd5299ceb0b7a1cc30ecc2f
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-11-24 12:28:21 +0100

    Crud operationer færdig

--- src/containers/Admin/ProductOverview.tsx
+++ src/containers/Admin/ProductOverview.tsx
@@ -1,14 +1,17 @@
 import PageLayout from '../../components/layout.tsx';
 import {ChannelType} from '../../types/channel.types.ts';
 import useProducts from '../../hooks/useProducts.ts';
-import {IProduct, INewProduct} from '../../types/products.types.ts';
+import {IProduct, INewProduct, IUpdateProduct} from '../../types/products.types.ts';
 import loading from '../../components/loading.tsx';
 import React, {useState} from 'react';
 import Creatable from 'react-select/creatable';
 
-function ProductOverview({channel}: { channel: ChannelType }) {
-    const {products, isLoading, destroy, create} = useProducts(channel);
-    const [showModal, setShowModal] = useState(false);
+function ProductOverview({channel}: {
+    channel: ChannelType
+}) {
+    const {products, isLoading, destroy, create, update} = useProducts(channel);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [showUpdateModal, setShowUpdateModal] = useState(false)
     const [newProductData, setNewProductData] = useState({
         name: '',
         price: 0,
@@ -17,7 +20,45 @@ function ProductOverview({channel}: { channel: ChannelType }) {
         categories: []
     });
     const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    const [selectedProduct, setSelectedProduct] = useState<IUpdateProduct | null>(null);
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+    const [productToDelete, setProductToDelete] = useState<IProduct | null>(null);
 
+
+    const handleEdit = (product: IProduct) => {
+        setSelectedProduct(product);
+        setNewProductData({
+            name: product.name,
+            price: product.price,
+            stock: product.stock,
+            channel: channel,
+            categories: [] /*change?*/
+        });
+        setSelectedCategories(product.categories.map(category => category.category.name));
+        setShowUpdateModal(true);
+    };
+
+    const handleUpdate = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+
+        const updatedProduct: IUpdateProduct = {
+            id: selectedProduct?.id || '',
+            name: newProductData.name,
+            price: newProductData.price,
+            stock: newProductData.stock,
+            channel: newProductData.channel
+        };
+
+        try {
+            await update(updatedProduct);
+            setShowUpdateModal(false);
+            setNewProductData({name: '', price: 0, stock: 0, categories: [], channel: channel});
+            setSelectedCategories([]);
+            setSelectedProduct(null);
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     const handleFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputField: string = e.target.name;
@@ -50,16 +91,35 @@ function ProductOverview({channel}: { channel: ChannelType }) {
 
         try {
             await create(newProduct);
-            setShowModal(false);
+            setShowCreateModal(false);
             setNewProductData({name: '', price: 0, stock: 0, categories: [], channel: channel});
         } catch (e) {
             console.error(e);
         }
     }
 
-    const handleDelete = (product: IProduct) => {
-        void destroy(product);
+    const openDeleteConfirmation = (product: IProduct) => {
+        setProductToDelete(product);
+        setShowDeleteConfirmation(true);
     };
+
+    const closeDeleteConfirmation = () => {
+        setProductToDelete(null);
+        setShowDeleteConfirmation(false);
+    };
+
+
+    const handleDelete = (product: IProduct) => {
+        openDeleteConfirmation(product);
+    };
+
+    const confirmDelete = async () => {
+        if (productToDelete) {
+            await destroy(productToDelete);
+            closeDeleteConfirmation();
+        }
+    };
+
 
     return (
         <PageLayout>
@@ -67,35 +127,35 @@ function ProductOverview({channel}: { channel: ChannelType }) {
                 <h2 className="text-3xl font-bold text-indigo-600 mb-4">Product List:</h2>
                 <button
                     className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
-                    onClick={() => setShowModal(true)}
+                    onClick={() => setShowCreateModal(true)}
                 >
-                    Create Product
+                    Opret Product
                 </button>
-                {showModal && (
+                {showCreateModal && (
                     <div className="fixed inset-0 flex items-center justify-center">
                         <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
                         <div className="bg-white p-8 rounded-md relative z-10">
                             <form onSubmit={handleSubmit}>
                                 <label className="block mb-4">
-                                    Name: <input className="border border-gray-300 p-2 w-full"
+                                    Navn: <input className="border border-gray-300 p-2 w-full"
                                                  type="text"
                                                  name="name"
                                                  onChange={handleFormInput}/>
                                 </label>
                                 <label className="block mb-4">
-                                    Price: <input className="border border-gray-300 p-2 w-full"
-                                                  type="number"
-                                                  name="price"
-                                                  onChange={handleFormInput}/>
+                                    Pris: <input className="border border-gray-300 p-2 w-full"
+                                                 type="number"
+                                                 name="price"
+                                                 onChange={handleFormInput}/>
                                 </label>
                                 <label className="block mb-4">
-                                    Stock: <input className="border border-gray-300 p-2 w-full"
+                                    Lager: <input className="border border-gray-300 p-2 w-full"
                                                   type="number"
                                                   name="stock"
                                                   onChange={handleFormInput}/>
                                 </label>
                                 <label className="block mb-4">
-                                    Categories:
+                                    Kategori:
                                     <Creatable
                                         isMulti={true}
                                         onChange={handleCategoryChange}
@@ -111,18 +171,96 @@ function ProductOverview({channel}: { channel: ChannelType }) {
                                     className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
                                     type="submit"
                                 >
-                                    Create
+                                    Opret
                                 </button>
                             </form>
                             <button
                                 className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 mt-4"
-                                onClick={() => setShowModal(false)}
+                                onClick={() => setShowCreateModal(false)}
+                            >
+                                Luk
+                            </button>
+                        </div>
+                    </div>
+                )}
+                {showUpdateModal && (
+                    <div className="fixed inset-0 flex items-center justify-center">
+                        <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
+                        <div className="bg-white p-8 rounded-md relative z-10">
+                            <form onSubmit={handleUpdate}>
+                                <label className="block mb-4">
+                                    Name: <input className="border border-gray-300 p-2 w-full"
+                                                 type="text"
+                                                 name="name"
+                                                 value={newProductData.name}
+                                                 onChange={handleFormInput}/>
+                                </label>
+                                <label className="block mb-4">
+                                    Price: <input className="border border-gray-300 p-2 w-full"
+                                                  type="number"
+                                                  name="price"
+                                                  value={newProductData.price}
+                                                  onChange={handleFormInput}/>
+                                </label>
+                                <label className="block mb-4">
+                                    Stock: <input className="border border-gray-300 p-2 w-full"
+                                                  type="number"
+                                                  name="stock"
+                                                  value={newProductData.stock}
+                                                  onChange={handleFormInput}/>
+                                </label>
+                                <label className="block mb-4">
+                                    Categories:
+                                    <Creatable
+                                        isMulti={true}
+                                        value={selectedCategories.map(category => ({value: category, label: category}))}
+                                        onChange={handleCategoryChange}
+                                        options={Array.from(new Set(products.flatMap(product =>
+                                            product.categories.map(category => category.category.name))))
+                                            .map((categoryName, index) => ({
+                                                value: categoryName,
+                                                label: categoryName,
+                                                key: index
+                                            }))}/>
+                                </label>
+                                <button
+                                    className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                                    type="submit"
+
+                                >
+                                    Update
+                                </button>
+                            </form>
+                            <button
+                                className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 mt-4"
+                                onClick={() => setShowUpdateModal(false)}
                             >
                                 Close
                             </button>
                         </div>
                     </div>
                 )}
+                {showDeleteConfirmation && (
+                    <div className="fixed inset-0 flex items-center justify-center">
+                        <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
+                        <div className="bg-white p-8 rounded-md relative z-10">
+                            <p className="mb-4">Er du sikker på, at du vil slette dette produkt?</p>
+                            <button
+                                className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 mr-2"
+                                onClick={confirmDelete}
+                            >
+                                Ja
+                            </button>
+                            <button
+                                className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+                                onClick={closeDeleteConfirmation}
+                            >
+                                Nej
+                            </button>
+                        </div>
+                    </div>
+                )}
+
 
                 {isLoading ? (
                     <loading.LoadingSpinner/>
@@ -153,10 +291,16 @@ function ProductOverview({channel}: { channel: ChannelType }) {
                                     </td>
                                     <td>
                                         <button
+                                            className="bg-green-500 text-white py-1 px-2 rounded hover:bg-green-600"
+                                            onClick={() => handleEdit(product)}
+                                        >
+                                            Rediger
+                                        </button>
+                                        <button
                                             className="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-600"
                                             onClick={() => handleDelete(product)}
                                         >
-                                            Delete
+                                            Slet
                                         </button>
                                     </td>
                                 </tr>
--- src/types/products.types.ts
+++ src/types/products.types.ts
@@ -2,41 +2,41 @@ import type {ChannelType} from "./channel.types.ts";
 import type {ICategory} from "./categories.types.ts";
 
 interface IProduct {
-	id: string;
-	name: string;
-	price: number;
-	stock: number;
-	channel: ChannelType;
-	categories: ICategory[];
-	createdAt: string;
-	updatedAt: string;
+    id: string;
+    name: string;
+    price: number;
+    stock: number;
+    channel: ChannelType;
+    categories: ICategory[];
+    createdAt: string;
+    updatedAt: string;
 }
 
 interface INewProduct {
-	id?: string;
-	name: string;
-	price: number;
-	stock: number;
-	channel: ChannelType;
-	categories: string[];
+    id?: string;
+    name: string;
+    price: number;
+    stock: number;
+    channel: ChannelType;
+    categories: string[];
 }
 
 interface IUpdateProduct {
-	id?: string;
-	name?: string;
-	price?: number;
-	stock?: number;
-	channel?: ChannelType;
+    id?: string;
+    name?: string;
+    price?: number;
+    stock?: number;
+    channel?: ChannelType;
 }
 
 export interface INewSaleProduct {
-	id: string;
-	quantity: number;
-	channel: ChannelType;
+    id: string;
+    quantity: number;
+    channel: ChannelType;
 }
 
 interface ISaleProduct {
-	product: IProduct;
+    product: IProduct;
 }
 
 export type {ISaleProduct, IUpdateProduct, INewProduct, IProduct};
