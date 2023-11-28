758883d822190a0b780503f5a0d00dd599d65d70
emilvn <emilvnielsen@hotmail.com>
2023-11-28 11:16:34 +0100

    moved create modal to seperate component

--- src/containers/Admin/ProductOverview.tsx
+++ src/containers/Admin/ProductOverview.tsx
@@ -3,11 +3,118 @@ import {ChannelType} from '../../types/channel.types.ts';
 import useProducts from '../../hooks/useProducts.ts';
 import {IProduct, INewProduct, IUpdateProduct} from '../../types/products.types.ts';
 import loading from '../../components/loading.tsx';
-import React, {useState} from 'react';
+import React, {ChangeEvent, Dispatch, SetStateAction, useState} from 'react';
 import Creatable from 'react-select/creatable';
 import toast, {Toaster} from 'react-hot-toast';
 import {FaCheck} from "react-icons/fa";
 import {IoCloseSharp} from "react-icons/io5";
+import type {ActionMeta, Options} from "react-select"
+
+interface INewProductData{
+    name: string;
+    amount: string;
+    price: number;
+    stock: number;
+    channel: ChannelType;
+    categories: string[]
+}
+interface ICreateModalProps{
+    selectedCategories: string[];
+    setShowCreateModal: Dispatch<SetStateAction<boolean>>;
+    setNewProductData: Dispatch<SetStateAction<INewProductData>>;
+    notifySuccess: (message:string) => string;
+    notifyError: (message:string) => string;
+    newProductData: INewProductData;
+    create:  (product: INewProduct) => Promise<void>;
+    channel:ChannelType;
+    handleFormInput: (e:ChangeEvent<HTMLInputElement>) => void;
+    handleCategoryChange: (newValue:Options<{ value:string }>, actionMeta:ActionMeta<{ value:string }>) => void;
+    products: IProduct[];
+}
+function CreateModal(props:ICreateModalProps){
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault()
+
+        const newProduct: INewProduct = {
+            name: props.newProductData.name + ", " + props.newProductData.amount,
+            price: props.newProductData.price,
+            stock: props.newProductData.stock,
+            channel: props.newProductData.channel,
+            categories: props.selectedCategories,
+        }
+
+        try {
+            await props.create(newProduct);
+            props.setShowCreateModal(false);
+            props.setNewProductData({name: '', amount: "", price: 0, stock: 0, categories: [], channel: props.channel});
+            props.notifySuccess('Produkt oprettet')
+        } catch (e) {
+            console.error(e);
+            props.notifyError('fejl ved oprettelse af produkt')
+        }
+    }
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center">
+            <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
+            <div className="bg-white p-8 rounded-md relative z-10">
+                <form onSubmit={handleSubmit}>
+                    <label className="block mb-4">
+                        Navn: <input className="border border-gray-300 p-2 w-full"
+                                     type="text"
+                                     name="name"
+                                     onChange={props.handleFormInput}/>
+                    </label>
+                    <label className="block mb-4">
+                        Mængde: <input className="border border-gray-300 p-2 w-full"
+                                       type="text"
+                                       name="amount"
+                                       onChange={props.handleFormInput}/>
+                    </label>
+                    <label className="block mb-4">
+                        Pris: <input className="border border-gray-300 p-2 w-full"
+                                     type="number"
+                                     name="price"
+                                     onChange={props.handleFormInput}/>
+                    </label>
+                    <label className="block mb-4">
+                        Lager: <input className="border border-gray-300 p-2 w-full"
+                                      type="number"
+                                      name="stock"
+                                      onChange={props.handleFormInput}/>
+                    </label>
+                    <label className="block mb-4">
+                        Kategori:
+                        <Creatable
+                            isMulti={true}
+                            onChange={props.handleCategoryChange}
+                            options={Array.from(new Set(props.products.flatMap(product =>
+                                product.categories.map(category => category.category.name))))
+                                .map((categoryName, index) => ({
+                                    value: categoryName,
+                                    label: categoryName,
+                                    key: index
+                                }))}/>
+                    </label>
+                    <div className="flex justify-around mt-20">
+                        <button
+                            className="bg-next-darker-orange text-next-blue py-2 px-4 rounded hover:bg-next-blue hover:text-next-orange"
+                            type="submit"
+                        >
+                            <FaCheck size={25}/>
+                        </button>
+                        <button
+                            className="bg-next-blue text-next-orange py-2 px-4 rounded hover:bg-next-darker-orange hover:text-next-blue ml-4"
+                            onClick={() => props.setShowCreateModal(false)}
+                        >
+                            <IoCloseSharp size={25}/>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
 
 function ProductOverview({channel}: {
     channel: ChannelType
@@ -15,7 +122,7 @@ function ProductOverview({channel}: {
     const {products, isLoading, destroy, create, update} = useProducts(channel);
     const [showCreateModal, setShowCreateModal] = useState(false);
     const [showUpdateModal, setShowUpdateModal] = useState(false)
-    const [newProductData, setNewProductData] = useState({
+    const [newProductData, setNewProductData] = useState<INewProductData>({
         name: '',
         amount: "",
         price: 0,
@@ -76,7 +183,7 @@ function ProductOverview({channel}: {
 
     //------------------ handle all form input both for create and update ------------------//
 
-    const handleFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFormInput = (e: ChangeEvent<HTMLInputElement>) => {
         const inputField: string = e.target.name;
         const inputFieldValue: string | number = e.target.type === 'number'
             ? Number(e.target.value)
@@ -91,33 +198,13 @@ function ProductOverview({channel}: {
 
     //------------------ create ------------------//
 
-    const handleCategoryChange = (newValue: any, actionMeta: any) => {
+    const handleCategoryChange = (newValue: Options<{ value:string }>, actionMeta: ActionMeta<{value:string}>) => {
         if (actionMeta.action === 'select-option' || actionMeta.action === 'create-option') {
-            setSelectedCategories(newValue.map((option: any) => option.value));
+            setSelectedCategories(newValue.map((option: {value:string}) => option.value));
         }
     };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-        e.preventDefault()
-
-        const newProduct: INewProduct = {
-            name: newProductData.name + ", " + newProductData.amount,
-            price: newProductData.price,
-            stock: newProductData.stock,
-            channel: newProductData.channel,
-            categories: selectedCategories,
-        }
-
-        try {
-            await create(newProduct);
-            setShowCreateModal(false);
-            setNewProductData({name: '', amount: "", price: 0, stock: 0, categories: [], channel: channel});
-            notifySuccess('Produkt oprettet')
-        } catch (e) {
-            console.error(e);
-            notifyError('fejl ved oprettelse af produkt')
-        }
-    }
+    
 
     //------------------ delete ------------------//
 
@@ -158,66 +245,19 @@ function ProductOverview({channel}: {
                 >
                     Opret Produkt
                 </button>
-                {showCreateModal && (
-                    <div className="fixed inset-0 flex items-center justify-center">
-                        <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
-                        <div className="bg-white p-8 rounded-md relative z-10">
-                            <form onSubmit={handleSubmit}>
-                                <label className="block mb-4">
-                                    Navn: <input className="border border-gray-300 p-2 w-full"
-                                                 type="text"
-                                                 name="name"
-                                                 onChange={handleFormInput}/>
-                                </label>
-                                <label className="block mb-4">
-                                    Mængde: <input className="border border-gray-300 p-2 w-full"
-                                                   type="text"
-                                                   name="amount"
-                                                   onChange={handleFormInput}/>
-                                </label>
-                                <label className="block mb-4">
-                                    Pris: <input className="border border-gray-300 p-2 w-full"
-                                                 type="number"
-                                                 name="price"
-                                                 onChange={handleFormInput}/>
-                                </label>
-                                <label className="block mb-4">
-                                    Lager: <input className="border border-gray-300 p-2 w-full"
-                                                  type="number"
-                                                  name="stock"
-                                                  onChange={handleFormInput}/>
-                                </label>
-                                <label className="block mb-4">
-                                    Kategori:
-                                    <Creatable
-                                        isMulti={true}
-                                        onChange={handleCategoryChange}
-                                        options={Array.from(new Set(products.flatMap(product =>
-                                            product.categories.map(category => category.category.name))))
-                                            .map((categoryName, index) => ({
-                                                value: categoryName,
-                                                label: categoryName,
-                                                key: index
-                                            }))}/>
-                                </label>
-                                <div className="flex justify-around mt-20">
-                                    <button
-                                        className="bg-next-darker-orange text-next-blue py-2 px-4 rounded hover:bg-next-blue hover:text-next-orange"
-                                        type="submit"
-                                    >
-                                        <FaCheck size={25}/>
-                                    </button>
-                                    <button
-                                        className="bg-next-blue text-next-orange py-2 px-4 rounded hover:bg-next-darker-orange hover:text-next-blue ml-4"
-                                        onClick={() => setShowCreateModal(false)}
-                                    >
-                                        <IoCloseSharp size={25}/>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                )}
+                {showCreateModal && (<CreateModal
+                    selectedCategories={selectedCategories}
+                    setShowCreateModal={setShowCreateModal}
+                    setNewProductData={setNewProductData}
+                    notifySuccess={notifySuccess}
+                    notifyError={notifyError}
+                    newProductData={newProductData}
+                    create={create}
+                    channel={channel}
+                    products={products}
+                    handleCategoryChange={handleCategoryChange}
+                    handleFormInput={handleFormInput}
+                />)}
                 {showUpdateModal && (
                     <div className="fixed inset-0 flex items-center justify-center">
                         <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur-md"></div>
