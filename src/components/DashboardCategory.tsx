adb46a26a0db576376012dafdcda5c605ef61cc8
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-11-30 12:59:52 +0100

    setup for barchart done

---
+++ src/components/DashboardCategory.tsx
@@ -0,0 +1,74 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import {Bar} from 'react-chartjs-2';
+import {ChannelType} from "../types/channel.types.ts";
+import {getCategories} from "../helpers/categories.ts";
+import useProducts from "../hooks/useProducts.ts";
+import {useState} from "react";
+import {ISale} from "../types/sales.types.ts";
+
+export function DashboardCategory({channel}: {
+    channel: ChannelType
+}) {
+    const {products} = useProducts(channel);
+    const [sales, setSales] = useState<ISale[]>([]);
+
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+    );
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: `Salg for hver Kategori for ${channel.toString()} hele året`,
+            },
+        },
+    };
+
+    const labels = getCategories(products);
+
+    const generateRandomData = () => labels.map(() => Math.floor(Math.random() * 1000));
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: channel.toString(),
+                data: generateRandomData(),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    };
+
+    return (
+        <Bar data={data} options={options}/>
+        /*<div>
+            <ul>
+                {sales.map(product => (
+                    <li key={product.id}>
+                        {product.products.map(category => (
+                            <span key={category.product.id}>{category.product.price}</span>
+                        ))}
+                    </li>
+                ))}
+            </ul>
+        </div>*/
+    );
+}
--- src/containers/Admin/Dashboard.tsx
+++ src/containers/Admin/Dashboard.tsx
@@ -1,12 +1,13 @@
 import PageLayout from "../../components/layout.tsx";
 import {ChannelType} from "../../types/channel.types.ts";
+import {DashboardCategory} from "../../components/DashboardCategory.tsx";
 
-function Dashboard({channel}:{channel:ChannelType}) {
-  return (
-    <PageLayout>
-      Dashboard - {channel}
-    </PageLayout>
-  )
+function Dashboard({channel}: { channel: ChannelType }) {
+    return (
+        <PageLayout>
+            <DashboardCategory channel={channel}/>
+        </PageLayout>
+    )
 }
 
 export default Dashboard
