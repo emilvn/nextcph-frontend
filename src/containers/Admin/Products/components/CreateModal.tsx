ca9e997946a2606f6b0fcc5c09dc95a15a062eda
emilvn <emilvnielsen@hotmail.com>
2023-12-09 21:11:16 +0100

    Revert "prettier"
    This reverts commit a5bb23fcd0445960342833c4d28b8977dc8b9ea4.

--- src/containers/Admin/Products/components/CreateModal.tsx
+++ src/containers/Admin/Products/components/CreateModal.tsx
@@ -1,54 +1,56 @@
-import type { Dispatch, SetStateAction } from "react";
-import type {
-  INewProduct,
-  IProduct,
-} from "../../../../types/products.types.ts";
-import ProductForm, { type IProductFormData } from "./ProductForm.tsx";
+import type {Dispatch, SetStateAction} from "react";
+import type {INewProduct, IProduct} from "../../../../types/products.types.ts";
+import ProductForm, {type IProductFormData} from "./ProductForm.tsx";
 import Modal from "../../../../components/modal.tsx";
-import type { ChannelType } from "../../../../types/channel.types.ts";
+import type {ChannelType} from "../../../../types/channel.types.ts";
+import type {ActionMeta, Options} from "react-select";
 import toast from "react-hot-toast";
-import { ICategoryState } from "../ProductOverview.tsx";
 
 interface IModalProps {
-  channel: ChannelType;
-  categoryState: ICategoryState;
+	channel: ChannelType;
+	categoryState: {
+		selectedCategories: string[];
+		setSelectedCategories: Dispatch<SetStateAction<string[]>>;
+		handleCategoryChange: (newValue: Options<{ value: string }>, actionMeta: ActionMeta<{ value: string }>) => void;
+	};
 }
 
 interface ICreateModalProps extends IModalProps {
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
-  create: (product: INewProduct) => Promise<void>;
-  products: IProduct[];
+	setIsOpenCreate: Dispatch<SetStateAction<boolean>>;
+	create: (product: INewProduct) => Promise<void>;
+	products: IProduct[];
 }
 
 function CreateModal(props: ICreateModalProps) {
-  const handleSubmit = async (data: IProductFormData): Promise<void> => {
-    const newProduct: INewProduct = {
-      name: `${data.name || ""}, ${data.amount || ""}`,
-      price: Number(data.price) || 0,
-      stock: Number(data.stock) || 0,
-      channel: props.channel,
-      categories: data.categories,
-    };
-    void props.create(newProduct);
-    props.setIsOpen(false);
-    toast.success("Produkt oprettet");
-    props.categoryState.setSelectedCategories([]);
-  };
+	const {categoryState, create, setIsOpenCreate, channel, products} = props;
+	const handleSubmit = async (data: IProductFormData): Promise<void> => {
+		const newProduct: INewProduct = {
+			name: `${data.name || ''}, ${data.amount || ''}`,
+			price: Number(data.price) || 0,
+			stock: Number(data.stock) || 0,
+			channel: channel,
+			categories: data.categories
+		}
+		void create(newProduct);
+		props.setIsOpenCreate(false);
+		toast.success('Produkt oprettet');
+		categoryState.setSelectedCategories([]);
+	}
 
-  return (
-    <Modal>
-      <ProductForm
-        setSelectedCategories={props.categoryState.setSelectedCategories}
-        title={"Tilføj Produkt"}
-        selectedCategories={props.categoryState.selectedCategories}
-        handleCategoryChange={props.categoryState.handleCategoryChange}
-        onSubmit={handleSubmit}
-        products={props.products}
-        setIsOpenModal={props.setIsOpen}
-      />
-    </Modal>
-  );
+	return (
+		<Modal>
+			<ProductForm
+				setSelectedCategories={categoryState.setSelectedCategories}
+				title={"Tilføj Produkt"}
+				selectedCategories={categoryState.selectedCategories}
+				handleCategoryChange={categoryState.handleCategoryChange}
+				onSubmit={handleSubmit}
+				products={products}
+				setIsOpenModal={setIsOpenCreate}
+			/>
+		</Modal>
+	);
 }
 
 export default CreateModal;
-export type { ICreateModalProps, IModalProps };
+export type {ICreateModalProps, IModalProps};
--- src/containers/Admin/Products/components/UpdateModal.tsx
+++ src/containers/Admin/Products/components/UpdateModal.tsx
@@ -1,51 +1,60 @@
-import type { IUpdateProduct } from "../../../../types/products.types.ts";
-import type { Dispatch, SetStateAction } from "react";
-import ProductForm, { type IProductFormData } from "./ProductForm.tsx";
+import type {INewProduct, IProduct, IUpdateProduct} from "../../../../types/products.types.ts";
+import type {Dispatch, SetStateAction} from "react";
+import ProductForm, {type IProductFormData} from "./ProductForm.tsx";
 import Modal from "../../../../components/modal.tsx";
-import type { IModalProps } from "./CreateModal.tsx";
+import type {IModalProps} from "./CreateModal.tsx";
 import toast from "react-hot-toast";
-import { IProductState } from "../ProductOverview.tsx";
 
 interface IUpdateModalProps extends IModalProps {
-  productState: IProductState;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
+	productState: {
+		products: IProduct[];
+		create: (product: INewProduct) => Promise<void>;
+		update: (product: IUpdateProduct) => Promise<void>;
+		destroy: (product: IProduct) => Promise<void>;
+		selectedProduct: IProduct | null;
+		setSelectedProduct: Dispatch<SetStateAction<IProduct | null>>;
+		productToDelete: IProduct | null;
+		setProductToDelete: Dispatch<SetStateAction<IProduct | null>>;
+	}
+	setIsOpenUpdate: Dispatch<SetStateAction<boolean>>;
 }
 
 function UpdateModal(props: IUpdateModalProps) {
-  const handleUpdate = async (data: IProductFormData): Promise<void> => {
-    if (!props.productState.selectedProduct) {
-      toast.error("Fejl ved redigering af produkt");
-      return;
-    }
-    const updatedProduct: IUpdateProduct = {
-      id: props.productState.selectedProduct.id,
-      name: `${data.name || ""}, ${data.amount || ""}`,
-      price: Number(data.price) || 0,
-      stock: Number(data.stock) || 0,
-      channel: props.channel,
-    };
-    void props.productState.update(updatedProduct);
-    props.setIsOpen(false);
-    props.categoryState.setSelectedCategories([]);
-    props.productState.setSelectedProduct(null);
-    toast.success("Produktet er opdateret");
-  };
+	const {productState, categoryState, channel, setIsOpenUpdate} = props;
+	const handleUpdate = async (data: IProductFormData): Promise<void> => {
+		if(!productState.selectedProduct) {
+			toast.error('Fejl ved redigering af produkt');
+			return;
+		}
+		const updatedProduct: IUpdateProduct = {
+			id: productState.selectedProduct.id,
+			name: `${data.name || ''}, ${data.amount || ''}`,
+			price: Number(data.price) || 0,
+			stock: Number(data.stock) || 0,
+			channel: channel
+		};
+		void productState.update(updatedProduct);
+		setIsOpenUpdate(false);
+		categoryState.setSelectedCategories([]);
+		productState.setSelectedProduct(null);
+		toast.success('Produktet er opdateret')
+	};
 
-  return (
-    <Modal>
-      <ProductForm
-        setSelectedCategories={props.categoryState.setSelectedCategories}
-        title={"Redigér Produkt"}
-        handleCategoryChange={props.categoryState.handleCategoryChange}
-        onSubmit={handleUpdate}
-        products={props.productState.products}
-        selectedProduct={props.productState.selectedProduct}
-        selectedCategories={props.categoryState.selectedCategories}
-        setIsOpenModal={props.setIsOpen}
-      />
-    </Modal>
-  );
+	return (
+		<Modal>
+			<ProductForm
+				setSelectedCategories={categoryState.setSelectedCategories}
+				title={"Redigér Produkt"}
+				handleCategoryChange={categoryState.handleCategoryChange}
+				onSubmit={handleUpdate}
+				products={productState.products}
+				selectedProduct={productState.selectedProduct}
+				selectedCategories={categoryState.selectedCategories}
+				setIsOpenModal={setIsOpenUpdate}
+			/>
+		</Modal>
+	);
 }
 
 export default UpdateModal;
-export type { IUpdateModalProps };
+export type {IUpdateModalProps};
